9c25c52ef1f82b6d64fe5907625e93a413ed08d5
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-05-07 21:52:41 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#Sass starter pack
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/*.html']).on('change', browserSync.reload);
+
+})
+
+gulp.task('default', ['serve']);
